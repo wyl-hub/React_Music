370cc06361584cf5fe1434dd8d5b662b7d590c75
wyl <wuyilong@karrytech.com>
2022-03-24 22:58:29 +0800

    swiper img

--- src/pages/FindMusic/FindMusic.jsx
+++ src/pages/FindMusic/FindMusic.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config';
 import styles from './findMusic.module.less'
 
 const Findmusic = (props) => {
     const routes = props.route.routes
+    const { pathname } = useLocation()
     return (
         <div>
             <div className={styles.headerNav}>
                 <NavLink className={styles.navLink} to="/findmusic/Recommend">
-                    <span className={styles.navText}>推荐</span>
+                    <span className={`${styles.navText} ${pathname.split('/')[2] === 'Recommend' ? styles.active : ''}`}>推荐</span>
                 </NavLink>
                 <NavLink className={styles.navLink} to="/findmusic/Rank">
-                    <span className={styles.navText}>排行榜</span>
+                    <span className={`${styles.navText} ${pathname.split('/')[2] === 'Rank' ? styles.active : ''}`}>排行榜</span>
                 </NavLink>
                 <NavLink className={styles.navLink} to="/findmusic/SongList">
-                    <span className={styles.navText}>歌单</span>
+                    <span className={`${styles.navText} ${pathname.split('/')[2] === 'SongList' ? styles.active : ''}`}>歌单</span>
                 </NavLink>
                 <NavLink className={styles.navLink} to="/findmusic/Station">
-                    <span className={styles.navText}>主播电台</span>
+                    <span className={`${styles.navText} ${pathname.split('/')[2] === 'Station' ? styles.active : ''}`}>主播电台</span>
                 </NavLink>
                 <NavLink className={styles.navLink} to="/findmusic/Singer">
-                    <span className={styles.navText}>歌手</span>
+                    <span className={`${styles.navText} ${pathname.split('/')[2] === 'Singer' ? styles.active : ''}`}>歌手</span>
                 </NavLink>
                 <NavLink className={styles.navLink} to="/findmusic/NewBorn">
-                    <span className={styles.navText}>新碟上架</span>
+                    <span className={`${styles.navText} ${pathname.split('/')[2] === 'NewBorn' ? styles.active : ''}`}>新碟上架</span>
                 </NavLink>
             </div>
-            <div className={styles.mainContent}>
+            <div>
                 {renderRoutes(routes)}
             </div>
         </div>
--- src/pages/FindMusic/pages/Recommend/pages/ReBanner/ReBanner.jsx
+++ src/pages/FindMusic/pages/Recommend/pages/ReBanner/ReBanner.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getBannerAction } from '../../store/actions'
 
@@ -6,32 +6,66 @@ import { Carousel } from 'antd';
 import styles from './reBanner.module.less'
 export default function ReBanner() {
 
-  // 获取仓库数据
-  const { banner } = useSelector(state => ({
-    banner: state.recommend.banner
-  }))
-
   // banner 数据初始化
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(getBannerAction())
   }, [dispatch])
 
+  // 获取仓库数据
+  const { banner = [] } = useSelector(state => ({
+    banner: state.recommend.banner
+  }))
+
+
+  // container 模糊背景图
+  const [bgImg, setBgImg] = useState('')
+
+  // ref hooks
+  const bannerRef = useRef()
+
+  // 每次轮播图改变之后 更改container背景图
+  const bannerChange = useCallback((from, to) => {
+    setBgImg(banner[to].imageUrl + '?imageView&blur=40x20')
+  }, [banner])
+
+  // 因为第一次可能数据还没请求过来  所有需要依赖这个数据的改变做初始化   
+  // 而且这个数据一般在组件加载只会请求一次
+  useEffect(() => {
+    if (banner.length > 0) {
+      setBgImg(banner[0].imageUrl + '?imageView&blur=40x20')
+    }
+  }, [banner])
+
 
   return (
-    <div className={styles.bannerContainer}>
-      <div className={styles.bannerLeft}>
-        <Carousel style={{ width: '730px' }} className={styles.swiper} autoplay effect="fade">
-          {
-            banner.map(item => (
-              <div className={styles.swiperItem}>
-                <img className={styles.swiperImg} src={item.imageUrl} />
-              </div>
-            ))
-          }
-        </Carousel>
+    <div className={styles.bannerContainer} style={{ backgroundImage: `url(${bgImg})` }}>
+      <div className={styles.bannerBox}>
+        {/* 左侧轮播图 */}
+        <div className={styles.bannerLeft}>
+          <Carousel
+            beforeChange={bannerChange}
+            style={{ width: '730px' }}
+            ref={bannerRef}
+            className={styles.swiper}
+            autoplay
+            effect="fade"
+          >
+            {
+              banner.map(item => (
+                <div key={item.encodeId} className={styles.swiperItem}>
+                  <img className={styles.swiperImg} src={item.imageUrl} />
+                </div>
+              ))
+            }
+          </Carousel>
+        </div>
+        {/* 右侧下载图片 */}
+        <div className={styles.bannerRight}></div>
+        {/* 按钮 */}
+        <div onClick={() => bannerRef.current.prev()} className={`${styles.btnLeft} ${styles.btn}`}></div>
+        <div onClick={() => bannerRef.current.next()} className={`${styles.btnRight} ${styles.btn}`}></div>
       </div>
-      <div className={styles.bannerRight}></div>
     </div>
   )
 }
